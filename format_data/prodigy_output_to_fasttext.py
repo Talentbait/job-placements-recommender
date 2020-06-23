07ff90ab1deb60e1d88c01ede36a507ef952f5e6
Magdiel <magdiel@Magdiels-MacBook-Pro.local>
2020-06-23 11:57:53 +0200

    Modified scripts to train on trainMode0 with 20 NN of each jobtitle as labels

--- format_data/prodigy_output_to_fasttext.py
+++ format_data/prodigy_output_to_fasttext.py
@@ -42,12 +42,19 @@ for job in validation_set.keys():
             validation_set[job].remove(example_id)
 
 used_data = {}
-with open('placement_to_jobtitle_classifier_normalized_examples_trainfile_2.json') as data_path:
+with open('placement_to_jobtitle_classifier_normalized_examples_trainfile_4.json') as data_path:
     data = json.load(data_path)
     used_data = data
 
 # with open('../unique_placements_dataset.json','w') as file_path:
 #     json.dump(ids_to_placement_dict,file_path,indent=4)
+
+labels_dict = {
+    'Erzieher':'__label__Erzieher __label__Erzieherinnen __label__Kita-Erzieher __label__Pädagogen __label__Erziehern __label__Kindererzieher __label__Kinderpfleger __label__Erzieherin __label__Kindergärtner __label__Heilerzieher __label__Kindergartenpädagogen __label__Lehrer __label__Arbeitserzieher __label__ErzieherInnen __label__Kita-Erzieherinnen __label__Sozialpädagogen __label__Horterzieher __label__Erziehers __label__Sozialarbeiter __label__Kindergärtnerinnen',
+    'Elektrotechniker':'__label__Elektrotechniker  __label__Elektroingenieur __label__Nachrichtentechniker __label__Elektrotechnik-Ingenieur __label__Elektriker __label__Elektrotechnikingenieur __label__Energietechniker __label__Elektromechaniker __label__Elektrotechnikerin __label__Elektro-Ingenieur __label__Informationstechniker __label__Ingenieur __label__Elektrotechnik __label__Elektrotechnikermeister __label__Elektrobetriebstechniker __label__Elektronikingenieur __label__Elektrotechnikern __label__Elektroniker __label__elektrotechniker __label__Elektroingenieure',
+    'Vertriebsmitarbeiter':'__label__Vertriebsmitarbeiter __label__Außendienstmitarbeiter __label__Vetriebsmitarbeiter __label__Vertriebsmitarbeitern __label__Vertriebsmitarbeiterin __label__Vertriebsingenieure __label__Vertriebsprofi __label__Vertriebsmitarbeiters __label__Außendienst __label__Vertriebler __label__Vertriebsspezialisten __label__Kundenbetreuer __label__Vertriebsingenieur __label__Vertriebsleiter __label__Außendienst-Mitarbeiter __label__Anwendungstechniker __label__Vertriebsmanager __label__Vertriebsassistenten __label__Kundenberater __label__Sales-Mitarbeiter',
+    'None':'__label__None0 __label__None1 __label__None2 __label__None3 __label__None4 __label__None5 __label__None6 __label__None7 __label__None8 __label__None9 __label__None10 __label__None11 __label__None12 __label__None13 __label__None14 __label__None15 __label__None16 __label__None17 __label__None18 __label__None19'
+}
 
 job_to_label_dict = {
     'Software-Entwickler':'None',
@@ -63,14 +70,20 @@ job_to_label_dict = {
     'Vertriebsmitarbeiter':'Vertriebsmitarbeiter'
 } 
 
-# with open('../Starspace/datasets/placement_to_jobtitle_classifier_normalized_examples_testfile_2.txt','w') as output:
-#     for job, val_set in validation_set.items():
-#         for example_id in val_set:
-#             if example_id not in used_data.keys():
-#                 placement = ids_to_placement_dict[example_id]
-#                 output.write(clean_description(placement['description'])[0] + ' __jobtitle__' + job_to_label_dict[job] + "\n")
+labeled_data_dict = {}
+with open('../Starspace/datasets/placement_to_jobtitle_classifier_normalized_examples_testfile_4.txt','w') as output:
+    for job, val_set in validation_set.items():
+        for example_id in val_set:
+            if example_id not in used_data.keys() and job not in ['Bankkaufmann','Personalreferent','Software-Entwickler','Auszubildende']:
+                placement = ids_to_placement_dict[example_id]
+                output.write(clean_description(placement['description'])[0] + ' __jobtitle__' + labels_dict[job_to_label_dict[job]] + "\n")
+                labeled_data_dict[example_id] = {
+                    'name':ids_to_placement_dict[example_id]['name'],
+                    'description':ids_to_placement_dict[example_id]['description'],
+                    'label':job_to_label_dict[job]
+                }
 
-# with open('../Starspace/datasets/placement_to_jobtitle_classifier_normalized_examples_trainfile_2.txt','w') as output:
+# with open('../Starspace/datasets/placement_to_jobtitle_classifier_normalized_examples_trainfile_4.txt','w') as output:
 #     labeled_data_dict = {}
 #     for example in labeled_data:
 #         example_id = example['meta']['id']
@@ -85,23 +98,20 @@ job_to_label_dict = {
 #                     'description':ids_to_placement_dict[example_id]['description'],
 #                     'label':label
 #                 }
-#                 output.write(clean_description(example['text'])[0] + " __jobtitle__" + str(label) + "\n")
-#         elif answer == 'reject':
-#             # output.write(clean_description(example['text'])[0] + " __jobtitle__None\n")
-#             pass
+#                 output.write(clean_description(example['text'])[0] + " " + labels_dict[label] + "\n")
 #     for job in [k for k, v in job_to_label_dict.items() if v == 'None']:
 #         for example_id in validation_set[job][:4]:
 #             example = ids_to_placement_dict[example_id]
-#             output.write(clean_description(example['description'])[0] + " __jobtitle__None\n")
+#             output.write(clean_description(example['description'])[0] + " " + labels_dict['None'] + "\n")
 #             labeled_data_dict[example_id] = {
 #                 'name':ids_to_placement_dict[example_id]['name'],
 #                 'description':ids_to_placement_dict[example_id]['description'],
 #                 'label':'None'
 #             }
 
-# data = pd.DataFrame.from_dict(labeled_data_dict).T
+data = pd.DataFrame.from_dict(labeled_data_dict).T
 
-# print(data)
-# print(data['label'].value_counts())
+print(data)
+print(data['label'].value_counts())
 
-# data.to_json("placement_to_jobtitle_classifier_normalized_examples_trainfile_2.json",indent=2,orient='index')
+# data.to_json("placement_to_jobtitle_classifier_normalized_examples_trainfile_4.json",indent=2,orient='index')
